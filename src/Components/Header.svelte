2d797354acbbf67bf2edad2935e2eefbf48e8838
Nkosinathi Milanzi <nathilanziv@gmail.com>
2024-08-28 01:57:01 +0200

    Add Navbar Toggle Functionality with State Management

---
+++ src/Components/Header.svelte
@@ -0,0 +1,18 @@
+<!-- Header.svelte -->
+<script>
+    // import Link from 'svelte-spa-router';
+
+    /**
+     * Boolean flag to control the visibility of the navbar.
+     * @type {boolean}
+     */
+    let showNavbar = false;
+
+    /**
+     * Toggles the visibility of the navbar by changing the value of `showNavbar`.
+     * If `showNavbar` is true, it sets it to false, and vice versa.
+     */
+    const toggleNavbar = () => {
+        showNavbar = !showNavbar;
+    };
+</script>
